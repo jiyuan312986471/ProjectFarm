dcf3b2e00a06ec7b4a3328d1bfc96fdf4fe40494
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-03-29 15:04:42 +0200

    with sql file

--- projectfarm.sql
+++ projectfarm.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015-03-18 14:08:33
+-- Generation Time: 2015-03-29 15:03:11
 -- 服务器版本： 5.6.15-log
 -- PHP Version: 5.5.8
 
@@ -23,108 +23,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 表的结构 `category`
---
-
-CREATE TABLE IF NOT EXISTS `category` (
-  `description` varchar(20) NOT NULL,
-  PRIMARY KEY (`description`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `document`
---
-
-CREATE TABLE IF NOT EXISTS `document` (
-  `idDocument` int(11) NOT NULL AUTO_INCREMENT,
-  `documentPath` varchar(1000) NOT NULL,
-  `added` date NOT NULL,
-  PRIMARY KEY (`idDocument`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `evaluation`
---
-
-CREATE TABLE IF NOT EXISTS `evaluation` (
-  `idProject` int(11) NOT NULL,
-  `riskLevel` int(11) NOT NULL,
-  `atractiveness` int(11) NOT NULL,
-  `emailEvaluator` varchar(100) NOT NULL,
-  PRIMARY KEY (`idProject`,`emailEvaluator`),
-  KEY `emailEvaluator` (`emailEvaluator`),
-  KEY `idProject` (`idProject`,`emailEvaluator`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `person`
---
-
-CREATE TABLE IF NOT EXISTS `person` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ssn` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `photo` longblob,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- 表的结构 `project`
 --
 
 CREATE TABLE IF NOT EXISTS `project` (
-  `idProject` int(11) NOT NULL AUTO_INCREMENT,
   `acronym` varchar(100) NOT NULL,
   `description` varchar(2000) NOT NULL,
-  `fundingDurationDays` int(11) NOT NULL,
   `budget` int(11) NOT NULL,
-  `created` date NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `emailOwner` varchar(100) NOT NULL,
   `category` varchar(20) NOT NULL,
-  PRIMARY KEY (`idProject`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`acronym`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- 转存表中的数据 `project`
 --
 
-INSERT INTO `project` (`idProject`, `acronym`, `description`, `fundingDurationDays`, `budget`, `created`, `emailOwner`, `category`) VALUES
-(1, 'test', 'test 2015.03.17 23:55', 0, 100, '2015-03-17', 'john@acme.com', 'Information Systems');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `email` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `userType` int(11) NOT NULL,
-  PRIMARY KEY (`email`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- 转存表中的数据 `user`
---
-
-INSERT INTO `user` (`email`, `name`, `password`, `userType`) VALUES
-('john@acme.com', 'John Silver', '123', 0),
-('mary@acme.com', 'Mary Moon', '123', 0),
-('paul@acme.com', 'Paul McDonalds', '123', 0),
-('sarah@geek.com', 'Sarah Logan', '456', 0),
-('thibault@geek.com', 'Thibault Moulin', '456', 0),
-('george@geek.com', 'George Papalodeminus', '456', 0);
+INSERT INTO `project` (`acronym`, `description`, `budget`, `created`, `emailOwner`, `category`) VALUES
+('test 0327 2208', 'test', 100, '2015-03-27 22:09:36', 'john@acme.com', 'Apps'),
+('test 0328 0010', 'test', 4564564, '2015-03-28 00:10:51', 'john@acme.com', 'Apps');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
